7d7aac55b728c4067685b7495d105b410855a28f
unknown <mg7984992@gmail.com>
2026-08-26 21:01:12 +0000

    Ajout React Router avec routes Login, Dashboard, Pointage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashboardPage'
 import PointagePage from './pages/PointagePage'
 
 function App() {
-  return <PointagePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/pointage" element={<PointagePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
